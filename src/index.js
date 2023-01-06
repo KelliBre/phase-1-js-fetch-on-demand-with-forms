1c0c138fd5bd4a5f6b7c8eea00a87ef53f095f5d
KelliBre <Kelli.Brecunier@gmail.com>
2023-01-06 14:24:33 +1000

    commit tree 876dd8c6a80f1ad57ca7a930ec2c8c77ab1a729a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        //Use prevent default method to override forms default behavior
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary =document.querySelector('section#movieDetails p');
+            console.log(title, summary);
+            title.innerText = data.title;
+            summary.innerText =data.summary;
+        })
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
